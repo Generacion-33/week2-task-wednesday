54ad828395aaf450a1c2ff63c44d1a45351cf750
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-09-06 19:54:10 -0300

    advances

---
+++ send.js
@@ -0,0 +1,16 @@
+/***************************/
+/* Ejercicios con bucles: */
+/**************************/
+
+// 1. Crea un arreglo con los números del 1 al 400 // Investigar el método .push para array
+
+// 2. Crea una función que reciba como parámetros 2 números, y retorne un arreglo con todos los valores en el rango de esos 2 números (incluyéndolos), por ejemplo, si recibe 1 y 4 la función debe de retornar el arreglo [1, 2, 3, 4], asumiremos que el segundo parámetro recibido es mayor al primero.
+
+// 3. Crea una función que lea un array de N cantidad de números y retorne la suma de todos esos números.
+
+// 4. Crea una función que reciba como parámetro un array de N cantidad de elementos y retorne el elemento que se encuentra en la mitad
+//    - En caso de que el número de la mitad sea uno solo el programa debe devolverlo, por ejemplo, del arreglo [1, 3, 4] devolvemos el 3
+//    - En caso de que hayan 2 números en medio devolveremos un array de números, por ejemplo, en el array [1, 3, 4, 5, 5, 3] devolveremos [4, 5]
+//! Investigar Math.floor ⚠️
+//* Math.floor ==> redondea hacia abajo. ⚠️
+//* Math.ceil ==> redondea hacia arriba. ⚠️
